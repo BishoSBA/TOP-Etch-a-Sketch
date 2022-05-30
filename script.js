bd9f2abc6f19c1576c9986c96a691c63b9fc8263
Beshir Saeed <beshir-saeed@live.com>
2022-05-30 13:02:04 +0300

    Progress with color changing functionailty

--- script.js
+++ script.js
@@ -3,30 +3,31 @@ const container = document.querySelector(".container");
 let grid = [];
 let defaultSize = 16;
 
-for (let i = 0; i < defaultSize; i++) {
-	grid[i] = [];
-	for (let j = 0; j < defaultSize; j++) {
-		grid[i].push(document.createElement("div"));
-		grid[i][j].classList.add("grid");
-		container.append(grid[i][j]);
-	}
-}
+drawGrid(defaultSize);
 
 const btn = document.querySelector(".start");
 btn.addEventListener("click", chooseSize);
 
+// prompts the user for a size number for the grid whe button is pressed
 function chooseSize() {
 	let size = defaultSize;
 	do {
-		size = window.prompt("Choose the size of the grid 16-100", 16);
+		size = window.prompt("Choose the size of the grid 1-100", 16);
 		size = parseInt(size);
-	} while (size >= 100);
+	} while (size >= 100 || size <= 0);
 
-	console.log(size);
-	reGrid(size);
+	drawGrid(size);
 }
 
-function reGrid(size) {
+// Generates random hex color value
+function randomColor() {
+	let color = Math.floor(Math.random() * 16777215);
+	color = "#" + color.toString(16);
+	return color;
+}
+
+// draws the drawing grid
+function drawGrid(size) {
 	const divs = document.querySelectorAll(".grid");
 	for (let div of divs) {
 		div.remove();
@@ -38,7 +39,9 @@ function reGrid(size) {
 		grid[i] = [];
 		for (let j = 0; j < size; j++) {
 			grid[i].push(document.createElement("div"));
+			grid[i][j].style.backgroundColor = randomColor();
 			grid[i][j].classList.add("grid");
+			//console.log(randomColor());
 			grid[i][j].style.width = `${width}%`;
 			grid[i][j].style.paddingBottom = `${width}%`;
 			container.append(grid[i][j]);
